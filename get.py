77afae56d2d4ff278c53f22ea6c2c2166a07ef7c
= <=>
2016-03-28 04:38:42 -0400

    Get message work

---
+++ get.py
@@ -0,0 +1,20 @@
+import sys
+from scapy.all import *
+
+f = open('inpartb.txt', 'r')
+
+src_ip = f.readline().rstrip()
+src_port = int(f.readline().rstrip())
+
+dst_ip = f.readline().rstrip()
+dst_port = int(f.readline().rstrip())
+
+get_body = f.read()
+
+ip = IP(src=src_ip, dst=dst_ip)
+tcp = TCP(sport=src_port, dport=dst_port)
+
+pkt = ip / tcp / get_body
+send(pkt)
+
+f.close()
